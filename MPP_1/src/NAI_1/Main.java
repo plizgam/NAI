8fa12e6725347bb4db33e9632833b1ead9e9751b
plizgam <48893871+plizgam@users.noreply.github.com>
2020-03-30 00:16:31 +0200

    Init final

---
+++ MPP_1/src/NAI_1/Main.java
@@ -0,0 +1,10 @@
+package NAI_1;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        new Knn("iris_training.txt","iris_test.txt");
+    }
+}
---
+++ MPP_1/src/NAI_1/Neighbour.java
@@ -0,0 +1,11 @@
+package NAI_1;
+
+public class Neighbour {
+    public String name;
+    public double distance;
+
+    public Neighbour(String name, double distance){
+        this.name = name;
+        this.distance = distance;
+    }
+}
